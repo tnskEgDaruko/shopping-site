ff4f72de4e7fcc4f07cffa04fa73fe8c7d2235f6
EC2 Default User <ec2-user@ip-172-31-67-224.ec2.internal>
2018-12-22 03:29:48 +0000

    product jQuery preview

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@ $(document).ready(function(){
         $("#hb_menu ul").slideToggle();
     });
 });
+
+$(window).load(function(){
+    $(".p_item img").click(function(){
+        var img_src = $(this).attr("src");
+        var img_alt = $(this).attr("alt");
+        
+        $("#preview img").attr({
+            src: img_src,
+            alt: img_alt
+        });
+
+    });
+});
